55a6629ac257364fc77b1668c3ce5fec730bfb68
pablomdd <pablo.mdd.007@gmail.com>
2020-05-27 22:40:26 -0500

    add Error Midddlewares

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const app = express();
 const { config } = require("./config/index");
 const moviesApi = require('./routes/movies');
 
+const {
+  logErrors,
+  errorHandler
+} = require('./utils/middleware/ErrorHandlers.js');
+
+
 // body-parser middleware
 app.use(express.json())
 
 moviesApi(app);
+
+// Error middlewares
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
